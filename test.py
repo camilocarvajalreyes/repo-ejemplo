998ef06924a9d84c65923fa8fee0abdf008176bd
camilocarvajalreyes <camilo.carvajal3009@gmail.com>
2022-08-28 18:48:31 -0400

    update test.py cambios con VS code

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
+import sys
 import numpy as np
-if __name__=="__main__":
-	print(np.random.normal())
 
+if __name__=="__main__":
+	mu = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+	sigma = float(sys.argv[2]) if len(sys.argv) > 2 else 1
+	print(np.random.normal(mu,sigma))
